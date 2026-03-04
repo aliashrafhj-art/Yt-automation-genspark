935246c173fecc27c0af915b47843fd84d4836ed
aliashrafhj-art <aliashrafhj@gmail.com>
2026-03-04 21:41:39 +0600

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from services.downloader import *
-from services.transcriber import *
-from services.ai_analyzer import *
-from services.video_processor import *
-from services.youtube_service import *
-from services.drive_service import *
-from services.scheduler_service import *
